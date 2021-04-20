c850f9c1d9ede29ae67613a3137067127bcc076d
siddharth-gotawade <82651347+siddharth-gotawade@users.noreply.github.com>
2021-04-20 16:41:21 +0530

    Division Functionality

---
+++ Calculator/Calculator/Calculator.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public int Addition(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+
+        public int Division(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return result;
+        }
+    }
+
+
+}
